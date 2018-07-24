b482148752875835cda2f39de291f2b5e7aae439
David Himmelstrup <lemmih@gmail.com>
2018-07-24 15:52:33 +0200

    tut03

---
+++ JS/tut03.js
@@ -0,0 +1,7 @@
+let shows = ['Westworld', 'Expanse', 'Criminal Minds'];
+
+console.log('shows =', shows);
+console.log('shows[0] =', shows[0]);
+console.log('shows[1] =', shows[1]);
+console.log('shows[2] =', shows[2]);
+console.log('shows[3] =', shows[3]);
